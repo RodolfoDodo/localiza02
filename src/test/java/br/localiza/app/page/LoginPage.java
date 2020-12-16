dab02fb1f06bfdec323ebab801cdb618c9adf151
Rodolfo Lacerda - BHS <bhs66@localiza.com>
2020-12-16 18:22:30 -0300

    Com Inicio

--- src/test/java/br/localiza/app/page/LoginPage.java
+++ src/test/java/br/localiza/app/page/LoginPage.java
@@ -5,6 +5,28 @@ import br.localiza.app.runners.DriverFactory;
 public class LoginPage {
 
 	public void with(String matricula, String senha) {
+		
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/botao_configuracoes").click();
+
+		DriverFactory.getDriver().findElementByXPath(
+				"/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout")
+				.click();
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/input_senha").sendKeys("111");
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/btn_continuar").click();
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/layout_input_patrimonio").click();
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/input_patrimonio").sendKeys("teste");
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/layout_input_agencia").click();
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/input_agencia").sendKeys("aabhz");
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/layout_configuracoes").click();
+
+		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/botaoSalvar").click();
 
 		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/input_matricula").sendKeys(matricula);
 		DriverFactory.getDriver().findElementById("com.localiza.menuapp:id/input_senha").sendKeys(senha);
